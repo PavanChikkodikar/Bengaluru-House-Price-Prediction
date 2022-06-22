7be19e36d7c06c30ce425de6a8efd12ead4c7ab7
PavanChikkodikar <pavanchikkodikar@gmail.com>
2022-06-22 22:47:50 +0530

    commit

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-
 app = Flask(__name__)
 
 data = pd.read_csv('Cleaned_data.csv')
@@ -25,7 +24,6 @@ def predict(): #method
     print(location,bhk ,bath, sqft)
     input = pd.DataFrame([[location, sqft, bath, bhk]],columns=['location', 'total_sqft', 'bath', 'bhk'])
     prediction = pipe.predict(input)[0] * 1e5 # multiplying with 100000
-
 
 
     return str(np.round(prediction,2))
